c5b3439aa7e216c929f80f1cae3a63c1594b8f0b
Michael-Bryant Choa <mbchoa@gmail.com>
2016-09-04 18:40:50 -0700

    setup store and react-redux provider bindings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import expenseApp from './redux-app/reducers';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
+let store = createStore(expenseApp);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
